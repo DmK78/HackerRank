2b9547aa8d5bb5af8d9178e0f19b03338ca5011a
Dmitry Kolganov <dmk78@inbox.ru>
2019-05-06 11:26:21 +0500

    Java Strings Introduction

--- src/JavaStringsIntroduction.java
+++ src/JavaStringsIntroduction.java
@@ -14,7 +14,7 @@ public class JavaStringsIntroduction {
             System.out.println("Yes");
         } else System.out.println("No");
         System.out.println(A.substring(0,1).toUpperCase()+A.substring(1)+" "+B.substring(0,1).toUpperCase()+B.substring(1));
-
+//
     }
 
 }
