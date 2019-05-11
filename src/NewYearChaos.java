7cdbaa3458915c4725dbfc138de346acb7a63cce
Dmitry Kolganov <dmk78@inbox.ru>
2019-05-11 13:10:15 +0500

    new

--- src/NewYearChaos.java
+++ src/NewYearChaos.java
@@ -34,6 +34,7 @@ public class NewYearChaos {
                         q[i + 1] = temp;
                         count++;
                         break;
+                        //
                     }
                 }
             }
